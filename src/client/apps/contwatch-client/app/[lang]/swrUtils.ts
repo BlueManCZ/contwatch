fae6fbf9f4d69aa2937b7551d7f9b62e1cfaaf7b
BlueManCZ <ivo97@centrum.cz>
2025-01-14 10:19:05 +0100

    Fix

--- src/client/apps/contwatch-client/app/[lang]/swrUtils.ts
+++ src/client/apps/contwatch-client/app/[lang]/swrUtils.ts
@@ -5,7 +5,6 @@ import { Endpoint } from "@repo/utils/endpoints";
 import { getApiEndpoint } from "@repo/utils/getApiEndpoint";
 import useSWR from "swr";
 
-
 export const useAttributeChart = (attributeIds: number[], date?: string) => {
     return useSWR<AttributeChartModel[]>(
         `${getApiEndpoint(Endpoint.attributeChart)}/${attributeIds.join(",")}/${attributeIds.length > 0 ? (date ?? "") : ""}`,
